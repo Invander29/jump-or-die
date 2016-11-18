758172e63408608dea06fc7eaece74a0c981e46b
kremes <mato.tomovic@gmail.com>
2016-11-18 05:00:18 +0100

    Pridane nahodne generovanie mapy, natocenie kamery

--- src/GameScene.cpp
+++ src/GameScene.cpp
@@ -1,3 +1,5 @@
+#include <cstdlib>
+#include <ctime>
 #include "GameScene.h"
 #include "Engine/Models/CubeTexture.h"
 #include "Engine/Core.h"
@@ -5,6 +7,8 @@
 #include "Engine/Models/CubeColor.h"
 #include "Engine/Models/FloorColor.h"
 #include "Engine/Models/FloorTexture.h"
+#include "Engine/Models/PlayerTexture.h"
+
 
 using namespace Scenes;
 
@@ -22,64 +26,126 @@ void GameScene::show() {
 
 	// Add camera object
 	Camera camera(45.0f, 0.1f, 100.0f);
-	camera.lookAt(glm::vec3(0.0f, 2.0f, 0.0f),  // Camera is in this position
-	              glm::vec3(0.0f, 0.0f, -4.0f));// Camera looks at this position)
+	camera.lookAt(glm::vec3(2.0f, 25.0f, 3.0f),  // Camera is in this position
+		glm::vec3(0.0f, 0.0f, -12.0f));// Camera looks at this position)
 	setCamera(camera);
+
+
 
 	// Add objects to scene
 	using namespace Models;
 	using namespace Graphics;
 
 	// Shaders for texture
-	GLuint progSimpleTexture = Core::GetShaderManager().createProgram("simple_texture", "simple_texture.vs", "simple_texture.fs");
+	progSimpleTexture = Core::GetShaderManager().createProgram("simple_texture", "simple_texture.vs", "simple_texture.fs");
 	spTexture texCube = Core::GetResourceManager().getTexture("cube.png");
 
-	// Cube with texture on position 0 0 -4
-	spCube cube = std::make_shared<CubeTexture>(progSimpleTexture, 1.0f, texCube->getId());
-	cube->setPosition(0, 1, -4);
-	add(cube);
+	player_pos = 1;
+
+	// Player
+	player = std::make_shared<PlayerTexture>(progSimpleTexture, 0.5f, texCube->getId());
+	player->setPosition(0, float(player_pos), -4);
+	add(player);
 
 	// Shaders for single color
-	GLuint progSimpleColor = Core::GetShaderManager().createProgram("simple_color", "simple_color.vs", "simple_color.fs");
+	progSimpleColor = Core::GetShaderManager().createProgram("simple_color", "simple_color.vs", "simple_color.fs");
 
-	// Floor with single color
-	spFloor floorColor = std::make_shared<FloorColor>(progSimpleColor, 3.0f, 1.0f, 0.0f, 0.0f, 1.0f, 0.0f);
-	floorColor->setPosition(0, 0, -4);
-	add(floorColor);
-
-	// Floor with texture
-	spTexture texFloor = Core::GetResourceManager().getTexture("floor.png");
+	texFloor = Core::GetResourceManager().getTexture("floor.png");
 	texFloor->setWrapping(GL_REPEAT, GL_REPEAT);
-	spFloor floorTexture = std::make_shared<FloorTexture>(progSimpleTexture, 3.0f, 1.0f, 0.0f, texFloor->getId());
-	floorTexture->setPosition(0, -1, -4);
-	add(floorTexture);
 
-	// Cube with single color
-	//add(std::make_shared<CubeColor>(progSimpleColor, 1.0f, 0.0f, 0.0f, 1.0f));
+	//Generate map
+	floors.push_back(std::make_shared<FloorTexture>(progSimpleTexture, 25.0f, 1.0f, 0.0f, texFloor->getId()));
+	floors.back()->setPosition(0, 0, (floors.size() - 1)*(-2.0));
+	add(floors.back());
+	floors.push_back(std::make_shared<FloorColor>(progSimpleColor, 25.0f, 1.0f, 0.0f, 0.6f, 0.6f, 0.6f));
+	floors.back()->setPosition(0, 0, (floors.size() - 1)*(-2.0));
+	add(floors.back());
+	floors.push_back(std::make_shared<FloorTexture>(progSimpleTexture, 25.0f, 1.0f, 0.0f, texFloor->getId()));
+	floors.back()->setPosition(0, 0, (floors.size() - 1)*(-2.0));
+	add(floors.back());
+	drawed_roads = 0;
+	srand(int(time(NULL)));
+	for (int i = 0; i < FLOORS_COUNT-3; i++)
+	{
+		drawOneFloor();
+	}
 }
 
 void GameScene::update() {
 	Scene::update();
 
-	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_O)) {
+	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_J)) {
 		getCamera().lookRight(-3 * Core::GetSpeed());
 
-	} else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_P)) {
-		getCamera().lookRight( 3 * Core::GetSpeed());
+	}
+	else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_L)) {
+		getCamera().lookRight(3 * Core::GetSpeed());
 	}
 
 	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_I)) {
-		getCamera().lookUp( 3 * Core::GetSpeed());
+		getCamera().lookUp(3 * Core::GetSpeed());
 
-	} else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_K)) {
+	}
+	else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_K)) {
 		getCamera().lookUp(-3 * Core::GetSpeed());
 	}
 
-	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_U)) {
+	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_O)) {
 		getCamera().lookSide(3 * Core::GetSpeed());
 
-	} else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_Y)) {
+	}
+	else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_P)) {
 		getCamera().lookSide(-3 * Core::GetSpeed());
+	}
+
+	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_A)) {
+		getCamera().moveRight(3 * Core::GetSpeed());
+
+	}
+	else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_D)) {
+		getCamera().moveRight(-3 * Core::GetSpeed());
+	}
+
+	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_W)) {
+		getCamera().moveUp(3 * Core::GetSpeed());
+
+	}
+	else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_S)) {
+		getCamera().moveUp(-3 * Core::GetSpeed());
+	}
+
+	if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_R)) {
+		getCamera().moveForward(9 * Core::GetSpeed());
+
+	}
+	else if (Core::GetKeyboard().isKeyPressed(SDL_SCANCODE_F)) {
+		getCamera().moveForward(-9 * Core::GetSpeed());
+	}
+
+	if (Core::GetKeyboard().isKeyTriggered(SDL_SCANCODE_SPACE)) {
+		player_pos++;
+		drawOneFloor();
+	}
+
+}
+
+void GameScene::drawOneFloor()
+{
+	using namespace Models;
+	using namespace Graphics;
+	if (drawed_roads < 3 && (drawed_roads == 0 || rand() % 2 == 0))
+	{
+		floors.push_back(std::make_shared<FloorColor>(progSimpleColor, 25.0f, 1.0f, 0.0f, 0.05 * (drawed_roads % 2) + 0.6, 0.05 * (drawed_roads % 2) + 0.6, 0.05 * (drawed_roads % 2) + 0.6));
+		floors.back()->setPosition(0, 0, (floors.size() - 1)*(-2.0));
+		add(floors.back());
+		drawed_roads++;
+	}
+	else
+	{
+		floors.push_back(std::make_shared<FloorTexture>(progSimpleTexture, 25.0f, 1.0f, 0.0f, texFloor->getId()));
+		floors.back()->setPosition(0, 0, (floors.size() - 1)*(-2.0));
+		add(floors.back());
+		drawed_roads = 0;
 	}
 }
 
