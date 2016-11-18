f2cb4e43b04455fe55f4916aa37048fe16e96887
kremes <mato.tomovic@gmail.com>
2016-11-18 05:01:19 +0100

    Pridane deklaracie globalnych premennych a funkci

--- src/GameScene.h
+++ src/GameScene.h
@@ -1,10 +1,26 @@
 #pragma once
 
+#include <vector>
 #include "Engine/Scenes/Scene.h"
+#include "Engine/Models/Floor.h"
+#include "Engine/Models/Player.h"
+#include "Engine/Core.h"
+
+#define FLOORS_COUNT 20 
 
 namespace Scenes {
 
 	class GameScene : public Scene {
+	private:
+		std::vector<Models::spFloor> floors;
+		Models::spPlayer player;
+		int player_pos;
+		int drawed_roads;
+		GLuint progSimpleColor;
+		GLuint progSimpleTexture;
+		Graphics::spTexture texFloor;
+		virtual void drawOneFloor();
+
 	public:
 		explicit GameScene();
 
