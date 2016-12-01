75649dc1f5e17c97df1934d9a73bef68fd17e73e
kremes <mato.tomovic@gmail.com>
2016-12-02 00:56:59 +0100

    Dorobena animacia skoku.

--- src/Player.cpp
+++ src/Player.cpp
@@ -1,10 +1,12 @@
+#include <cmath>
 #include "Player.h"
 #include "Engine/Input/Keyboard.h"
 #include "Engine/Application.h"
 
 Player::Player(GLuint program)
-	: CubeColor(program, 1.0f, 1.0f, 1.0f, 1.0f)
+	: CubeColor(program, 0.8f, 1.0f, 1.0f, 1.0f)
 {
+	jumpAnimation = -1;
 }
 
 Player::~Player()
@@ -20,7 +22,25 @@ void Player::update()
 {
 	Input::Keyboard& keyboard = Application::instance().keyboard();
 	if (keyboard.isKeyTriggered(GLFW_KEY_SPACE)) {
-		moveZ(-2.0f);
+		if (jumpAnimation > -1) //ak sa nestihne dokoncit animacia
+		{
+			moveZ(-2.0 / ANIM_STEPS * (ANIM_STEPS - jumpAnimation));
+			setZ(round(z()));
+			setY(0.0);
+		}
+		jumpAnimation = 0;
 		mJumpPosition++;
+	}
+	if (jumpAnimation > -1 && jumpAnimation<ANIM_STEPS) //animacia skoku
+	{
+		moveZ(-2.0/ ANIM_STEPS);
+		moveY(jumpAnimation<(ANIM_STEPS/2) ? (4.0 / (ANIM_STEPS / 2)) : (-4.0 / (ANIM_STEPS / 2)));
+		jumpAnimation++;
+	}
+	else
+	{
+		jumpAnimation = -1;
+		setZ(round(z()));
+		setY(0.0);
 	}
 }
